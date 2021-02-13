a650fa7e6812a8257ea5e2c77288550fceca60a3
User <User@DESKTOP-0BJFLEG>
2021-02-13 11:48:47 +0200

    Add project files.

---
+++ HW_EntityFramework/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (OrderDBEntities orderEntities = new OrderDBEntities())
+            {
+                List<Product> products = (from p in orderEntities.Products
+                                select p).ToList();
+
+                List<Product> productsMS = orderEntities.Products.ToList();
+
+
+                Console.WriteLine();
+
+                foreach  (var p in products)
+                {
+                    Console.WriteLine(p.NAME);
+                }
+            }
+
+        }
+    }
+}
